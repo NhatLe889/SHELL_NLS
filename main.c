20a8d6634cf654ad3419ba2e0064d975d21555e5
Nhat Le <nhatle@Nhats-MacBook-Pro-2.local>
2024-08-14 01:10:14 -0400

    delete my own directory change

--- main.c
+++ main.c
@@ -8,11 +8,7 @@
 #include "nls_command.h"
 
 int main(int argc, char **argv) {
-    if (chdir("/Users/nhatle") != 0) {
-        perror("starting shell fail at home directory");
-        exit(EXIT_FAILURE);
-    }
-
+    
     //Call shell_loop to keep shell running
     shell_loop();
 
